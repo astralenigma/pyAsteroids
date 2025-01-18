7f44bc61fbd6ba0ec57f2ed495f6ba5690fdfb2d
Astralenigma <astralenigma@hotmail.com>
2025-01-18 19:16:22 +0000

    Set game to 60 FPS

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import constants as c
 def main():
     print("Starting asteroids!")
     screen = display.set_mode((c.SCREEN_WIDTH,c.SCREEN_HEIGHT))
-
+    clock=pygame.time.Clock()
+    dt=0
     while(True):
         for event in event_man.get():
             if event.type==pygame.QUIT:
                 return
         screen.fill(color="black")
         display.flip()
-
+        dt=clock.tick(60)/1000
 
 if __name__=="__main__":
     main()
