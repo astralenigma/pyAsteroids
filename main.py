8ad6f2d1550e9321daae76ffde759728f8b3eef7
Astralenigma <astralenigma@hotmail.com>
2025-01-18 15:35:16 +0000

    Added a black window that can be closed.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pygame
-import constants
+import pygame.display as display
+import pygame.event as event_man
+import constants as c
 def main():
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    screen = display.set_mode((c.SCREEN_WIDTH,c.SCREEN_HEIGHT))
+
+    while(True):
+        for event in event_man.get():
+            if event.type==pygame.QUIT:
+                return
+        screen.fill(color="black")
+        display.flip()
 
 
 if __name__=="__main__":
