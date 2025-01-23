4c0a7c4e13a16f64d05d7fd0e7a2ea11176e2c71
Astralenigma <astralenigma@hotmail.com>
2025-01-23 20:22:13 +0000

    Changed game logic to use groups

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ import pygame.event as event_man
 import constants as c
 import player
 def main():
+    updatable=pygame.sprite.Group()
+    drawable=pygame.sprite.Group()
+    player.Player.containers=(updatable,drawable)
     print("Starting asteroids!")
     screen = display.set_mode((c.SCREEN_WIDTH,c.SCREEN_HEIGHT))
     clock=pygame.time.Clock()
     dt=0
     player1=player.Player(c.SCREEN_WIDTH/2,c.SCREEN_HEIGHT/2)
+    
     while(True):
         for event in event_man.get():
             if event.type==pygame.QUIT:
                 return
-        player1.update(dt)
+        for gameobject in updatable:
+            gameobject.update(dt)
+        
         screen.fill(color="black")
-        player1.draw(screen)
+        for gameobject in drawable:
+            gameobject.draw(screen)
         display.flip()
         dt=clock.tick(60)/1000
 
