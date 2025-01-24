eb6793efe23da135135871839805f186116866e2
Astralenigma <astralenigma@hotmail.com>
2025-01-24 16:36:51 +0000

    Slowdown the ability to shoot bubbles

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player(CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation=0
+        self.timer=0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -25,12 +26,15 @@ class Player(CircleShape):
         forward=pygame.Vector2(0,1).rotate(self.rotation)
         self.position+=forward*PLAYER_SPEED*dt
     def shoot(self):
+        if self.timer>0:
+            return
         shot=Shot(self.position.x,self.position.y)
         shot.velocity=pygame.Vector2(0,1).rotate(self.rotation)*PLAYER_SHOT_SPEED
+        self.timer=PLAYER_SHOOT_COOLDOWN
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
-
+        self.timer-=dt
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
