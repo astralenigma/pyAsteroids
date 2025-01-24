b23a2196ce8ba78e062b4050c9e5d4c85087483d
Astralenigma <astralenigma@hotmail.com>
2025-01-24 16:47:40 +0000

    Made asteroids more resilient

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import *
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
@@ -8,3 +10,14 @@ class Asteroid(CircleShape):
         pygame.draw.circle(screen,"white",self.position,self.radius,2)
     def update(self, dt):
         self.position+=self.velocity*dt
+    def split(self):
+        self.kill()
+        if self.radius<=ASTEROID_MIN_RADIUS:
+            return
+        angle=random.uniform(20,50)
+        vector_angle1=self.velocity.rotate(angle)
+        vector_angle2=self.velocity.rotate(-angle)
+        ast1=Asteroid(self.position.x,self.position.y,self.radius-ASTEROID_MIN_RADIUS)
+        ast1.velocity=vector_angle1
+        ast2=Asteroid(self.position.x,self.position.y,self.radius-ASTEROID_MIN_RADIUS)
+        ast2.velocity=vector_angle2
--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collides_with(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
                     continue
             if asteroid.collides_with(player1):
