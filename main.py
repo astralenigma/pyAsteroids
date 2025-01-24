b6719c2b1f27ba2a5d2f0ab28437446d2556dcd1
Astralenigma <astralenigma@hotmail.com>
2025-01-24 16:39:35 +0000

    Switched bubbles for bullets

--- main.py
+++ main.py
@@ -29,6 +29,11 @@ def main():
         for gameobject in updatable:
             gameobject.update(dt)
         for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.kill()
+                    shot.kill()
+                    continue
             if asteroid.collides_with(player1):
                 print("Game over!")
                 sys.exit()
